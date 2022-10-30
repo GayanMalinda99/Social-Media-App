ab7cc7bba45c24f8baccb13a487a3fc8b9e0866a
GayanMalinda99 <gayan1999malinda@gmail.com>
2022-10-30 15:49:15 +0530

    Fetch data to Profile

--- client-ui/src/pages/profile/Profile.jsx
+++ client-ui/src/pages/profile/Profile.jsx
@@ -4,19 +4,20 @@ import RightBar from "../../components/rightBar/RightBar";
 import Feed from "../../components/feed/Feed";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router";
 
 export default function Home() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({})
+  const username = useParams().username;
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`/users?userId=${post.userId}`);
+      const res = await axios.get(`/users?username=${username}`);
       setUser(res.data);
     };
     fetchUser();
-  }, [post.userId]);
-
+  }, [username]);
 
   return (
     <>
@@ -28,23 +29,31 @@ export default function Home() {
           <div className="profileCover">
             <img
               className="profileCoverImg"
-              src="asserts/post/3.jpeg"
+              src={
+                user.coverPicture
+                  ? PF + user.coverPicture
+                  : PF + "person/noCover.png"
+              }
               alt=""
             />
             <img
               className="profileUserImg"
-              src="asserts/person/1.jpeg"
+              src={
+                user.profilePicture
+                  ? PF + user.profilePicture
+                  : PF + "person/noAvatar.png"
+              }
               alt=""
             />
           </div>
           <div className="profileInfo">
-              <h4 className="profileInfoName">Safak Kocaoglu</h4>
-              <span className="profileInfoDesc">Hello my friends!</span>
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
           </div>
           </div>
           <div className="profileRightBottom">
-              <Feed/>
-              <RightBar profile/>
+              <Feed username={username} />
+              <RightBar user={user} />
           </div>
         </div>
       </div>
